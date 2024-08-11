e829abbc682571392d3f2589ea068eabab4781c7
hesam-sa <hesam.sadat@gmail.com>
2024-08-11 19:23:57 +0330

    move html pages
    move templates and customize them

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('',index_view)
+    path('',index_view,name='home'),
+    path('contact',contact_view,name='contact'),
+    path('about',about_view,name='about'),
+    path('features',feature_view,name='features'),
 
 ]

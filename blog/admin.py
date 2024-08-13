312921f7147d9968fd86f7ba690bf54ae4dd151e
hesam-sa <hesam.sadat@gmail.com>
2024-08-13 13:53:25 +0330

    post model and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'published_date'
+    list_display = ('id','title','counted_view','status','created_date','login_required')
+    list_filter = ('status','login_required')
+    search_fields = ('title','content')
+    ordering = ('id',)
+
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(help_text='type something')
+    #image
+    #catagory
+    #author
+    #tag
+    counted_view = models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+    login_required = models.BooleanField(default=False)
+  
+
+    class Meta:
+        verbose_name_plural = 'پست'
+
+    def __str__(self):
+        return self.title

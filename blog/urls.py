4d815b2c18b04b7fdbec1cec11309196a720ab6d
hesam-sa <hesam.sadat@gmail.com>
2024-08-14 20:09:21 +0330

    connect blog to single blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('',blog_view,name='blog'),
-    path('single',single_view,name='single'),
+    path('<int:pid>',single_view,name='single'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
 
 # Create your views here.
 def blog_view(request):
-    return render(request,'blog/index.html')
+    posts = Post.objects.filter(status=True)
+    context = {'posts':posts}
+    return render(request,'blog/index.html',context)
 
-def single_view(request):
-    return render(request,'blog/blog-single.html')
+def single_view(request,pid):
+    post=get_object_or_404(Post,pk=pid,status=1)
+    context={'post':post}
+    return render(request,'blog/blog-single.html',context)

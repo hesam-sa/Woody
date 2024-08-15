4edb015fb6d46f20059f76b48994f49bcf2b114c
hesam-sa <hesam.sadat@gmail.com>
2024-08-15 19:22:28 +0330

    next-prev app

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+from next_prev import next_in_order, prev_in_order
 
 # Create your views here.
 def blog_view(request):
@@ -9,5 +10,14 @@ def blog_view(request):
 
 def single_view(request,pid):
     post=get_object_or_404(Post,pk=pid,status=1)
-    context={'post':post}
+    qs=Post.objects.filter(status=1).order_by("pk")
+    next = next_in_order(post,qs=qs)
+    if next:
+            nx=next
+    else: nx=post
+    prev = prev_in_order(post,qs=qs)
+    if prev:
+            pr=prev
+    else: pr=post
+    context={'post':post,'next':nx,'prev':pr}
     return render(request,'blog/blog-single.html',context)

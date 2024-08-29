1d84015c7b642b52806f93ee2ea6bec005518508
hesam-sa <hesam.sadat@gmail.com>
2024-08-29 19:46:52 +0330

    Update settings.py

--- woodysite/settings.py
+++ woodysite/settings.py
@@ -147,7 +147,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'hesam.sadat@gmail.com'
 EMAIL_HOST_PASSWORD = 'fnus kfdp twvr mmbe'
 
-
+#captcha Configuration 
 CAPTCHA_FONT_SIZE = 52
 CAPTCHA_CHALLENGE_FUNCT = 'captcha.helpers.math_challenge'
 CAPTCHA_LETTER_ROTATION = (-5,5)

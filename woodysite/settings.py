f887c369b64fec9e2f8072e647e3f30d07a9c8d4
hesam-sa <hesam.sadat@gmail.com>
2025-08-15 23:11:22 +0330

    settings

--- woodysite/settings.py
+++ woodysite/settings.py
@@ -138,7 +138,8 @@ SECRET_KEY = 'django-insecure-y=sczm)r@)2go_j+nq5=8v(v1z9t+sghin@+jnewqf7*@th=+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hesamsa.pythonanywhere.com', '127.0.0.1', 'localhost']
+
 
 #site framwork
 SITE_ID = 2

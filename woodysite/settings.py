7971e8a83ef7e07893b58270b48527fcc8a6e784
hesam-sa <hesam.sadat@gmail.com>
2025-08-15 16:01:29 +0330

    Update settings.py

--- woodysite/settings.py
+++ woodysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y=sczm)r@)2go_j+nq5=8v(v1z9t+sghin@+jnewqf7*@th=+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
